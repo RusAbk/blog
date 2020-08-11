bd2491497d564a62f41314195bc20e7e2073792d
Ruslan Abkadirov <rusabk@yandex.ru>
2020-08-11 23:16:42 +0300

    button component added

---
+++ blog/.vuepress/components/Btn.vue
@@ -0,0 +1,30 @@
+<template>
+    <router-link :to="href" class="btn">
+        <slot></slot>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: "Btn",
+    props: ['href']
+}
+</script>
+
+<style scoped>
+    .btn{
+        display: inline-block;
+        border:1px solid #3eaf7c;
+        color: #3eaf7c;
+        padding: 10px 25px;
+        border-radius: 5px;
+        transition: .3s;
+        font-weight: 700;
+        line-height: 1;
+    }
+    .btn:hover{
+        text-decoration: none !important;
+        color: #fff;
+        background-color: #3eaf7c;
+    }
+</style>
