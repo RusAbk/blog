62e189f536daa58d6d482c6e1be7f9f7021f4073
Ruslan Abkadirov <rusabk@yandex.ru>
2020-08-11 12:44:27 +0300

    code became more readable

--- blog/.vuepress/components/BlogIndex.vue
+++ blog/.vuepress/components/BlogIndex.vue
@@ -1,23 +1,28 @@
 <template>
-<div>
+  <div>
     <div v-for="post in posts">
-        <h3>
-            {{ post.frontmatter.title }}
-        </h3>
-        <p>{{ post.frontmatter.description }}</p>
-        <p><router-link :to="post.path">Read more</router-link></p>
+      <h3>{{ post.frontmatter.title }}</h3>
+      <p>{{ post.frontmatter.description }}</p>
+      <p>
+        <router-link :to="post.path">Read more</router-link>
+      </p>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    computed: {
-        posts() {
-            return this.$site.pages
-                .filter(x => x.path.startsWith('/posts/') && !x.frontmatter.blog_index)
-                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
-        }
-    }
-}
+  computed: {
+    posts() {
+      return this.$site.pages
+        .filter(
+          (x) =>
+            x.path.startsWith("/posts/") && x.frontmatter.blog_index != true
+        )
+        .sort(
+          (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+        );
+    },
+  },
+};
 </script>
