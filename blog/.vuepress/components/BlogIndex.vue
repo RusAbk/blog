5bccc640d76dbfe6863a0bfc74d3ff0bc54a0388
Ruslan Abkadirov <rusabk@yandex.ru>
2020-08-24 00:35:40 +0300

    added post info like author and date

--- blog/.vuepress/components/BlogIndex.vue
+++ blog/.vuepress/components/BlogIndex.vue
@@ -2,6 +2,7 @@
   <div>
     <div v-for="post in posts" class="preview-container">
       <h3 class="preview-title"><router-link :to="post.path">{{ post.frontmatter.title }}</router-link></h3>
+      <small class="preview-info">👤 {{ post.frontmatter.author }} | 🗓 {{formatDate(post.frontmatter.date)}}</small>
       <p>{{ post.frontmatter.description }}</p>
       <p>
         <Btn :href="post.path">Читать далее {{post.frontmatter.emoji}}</Btn>
@@ -23,8 +24,19 @@ export default {
         .sort(
           (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
         );
-    },
+    }
   },
+  methods: {
+    formatDate(dateString) {
+      let date = new Date(dateString);
+      console.log(this.data);
+      let day = date.getDate()<10 ? '0' : '';
+      day += date.getDate();
+      let month = date.getMonth()<10 ? '0' : '';
+      month += date.getMonth();
+      return `${day}.${month}.${date.getFullYear()}`;
+    }
+  }
 };
 </script>
 
@@ -39,5 +51,10 @@ export default {
 
   .preview-title a {
     color: #2c3e50 !important;
+  }
+  
+  .preview-info {
+    margin: 15px 0 0;
+    display: block;
   }
 </style>
