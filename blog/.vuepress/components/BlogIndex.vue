a49ef7abe714ea84593637435cb6d7cb255c5d91
Ruslan Abkadirov <rusabk@yandex.ru>
2020-08-24 01:37:03 +0300

    tags added

--- blog/.vuepress/components/BlogIndex.vue
+++ blog/.vuepress/components/BlogIndex.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <h1>Последние публикации</h1>
+    <div>
+      <div>
+        <label class="tag" :class="selectedTags.indexOf(tag) != -1 ? 'selected':''" v-for="tag in tags">
+          <input type="checkbox" v-model="selectedTags" :value="tag"> {{tag}}
+        </label>
+      </div>
+    </div>
     <div v-for="post in posts" class="preview-container">
       <h3 class="preview-title"><router-link :to="post.path">{{ post.frontmatter.title }}</router-link></h3>
       <small class="preview-info">👤 {{ post.frontmatter.author }} | 🗓 {{formatDate(post.frontmatter.date)}}</small>
@@ -14,22 +22,52 @@
 <script>
 import Btn from "./Btn.vue";
 export default {
+  data(){
+    return {
+      selectedTags: []
+    }
+  },
   computed: {
     posts() {
       return this.$site.pages
         .filter(
-          (x) =>
-            x.path.startsWith("/posts/") && x.frontmatter.blog_index != true
+          (post) => {
+            let hasTag = this.selectedTags.length == 0 ? true : false;
+            if(!hasTag && post.path.startsWith("/posts/") && !post.frontmatter.blog_index)
+              for(let tag of post.frontmatter.tags){
+                if(this.selectedTags.indexOf(tag) != -1){
+                  hasTag = true;
+                  break;
+                }
+              }
+            console.log(post.frontmatter.title + ' ' + hasTag);
+
+            return post.path.startsWith("/posts/") && !post.frontmatter.blog_index && hasTag;
+          }
         )
         .sort(
           (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
         );
+    },
+    tags(){
+      let posts = this.$site.pages
+        .filter(
+          (x) =>
+            x.path.startsWith("/posts/") && x.frontmatter.blog_index != true
+        );
+
+      let tagsList = [];
+      for(let page of posts){
+        for(let tag of page.frontmatter.tags){
+          if(tagsList.indexOf(tag) == -1) tagsList.push(tag);
+        }
+      }
+      return tagsList;
     }
   },
   methods: {
     formatDate(dateString) {
       let date = new Date(dateString);
-      console.log(this.data);
       let day = date.getDate()<10 ? '0' : '';
       day += date.getDate();
       let month = date.getMonth()<10 ? '0' : '';
@@ -56,5 +94,22 @@ export default {
   .preview-info {
     margin: 15px 0 0;
     display: block;
+  }
+
+  .tag {
+    display: inline-block;
+    margin: 0 10px 10px 0;
+    padding: 2px 6px;
+    border-radius: 4px;
+    cursor: pointer;
+    border: 1px solid #3eaf7c;
+    color: #3eaf7c;
+  }
+  .tag.selected{
+    background: #3eaf7c;
+    color: #fff;
+  }
+  .tag input{
+    display: none;
   }
 </style>
