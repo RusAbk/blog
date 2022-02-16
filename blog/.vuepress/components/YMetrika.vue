4e237de1b010752fddd9a763adfb9ee363931f83
Ruslan Abkadirov <rusabk@yandex.ru>
2022-02-16 07:58:06 +0300

    webvisor added to yandex metrica code

--- blog/.vuepress/components/YMetrika.vue
+++ blog/.vuepress/components/YMetrika.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- Yandex.Metrika counter -->
+<!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -9,8 +9,7 @@
    ym(66397087, "init", {
         clickmap:true,
         trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
+        accurateTrackBounce:true
    });
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/66397087" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
