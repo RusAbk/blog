e453e2485ed5efa0fca0d0218465bf8408d873fe
Ruslan Abkadirov <rusabk@yandex.ru>
2026-05-21 14:53:13 +0700

    Add Microsoft Clarity tracking

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -35,11 +35,20 @@ export default (() => {
       (e) => e.name === CustomOgImagesEmitterName,
     )
     const ogImageDefaultPath = `https://${cfg.baseUrl}/static/og-image.png`
+    const clarityScript = `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wugylym735");`
 
     return (
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{ __html: clarityScript }}
+        ></script>
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
